3bf91a8baaff8ce279ba1b9311420fceb31af6a5
Petr <pvmigdalov@yandex.ru>
2026-01-06 19:36:59 +0300

    Поменял комаду запуска uvicorn в main.py

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ async def redirect_to(
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=8001,
         reload=True
